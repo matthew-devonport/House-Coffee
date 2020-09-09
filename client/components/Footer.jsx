e447d5cac6d21fbee5f6d40fbeb392e67a85a13c
matthew-devonport <roarsintheeast@gmail.com>
2020-09-09 23:16:00 +1200

    scaled home page down to 450px

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -9,10 +9,11 @@ class Footer extends React.Component {
             <React.Fragment>
                 <div className="footerFlex">
                     <div className="footerLink"><a href="mailto:housecoffee@xtra.co.nz">housecoffee@xtra.co.nz</a></div>
-                    <div className="footerInfo">Copyright &copy; 2020 House Coffee. All rights reserved.<br />
-                     Website design and build by <a href="https://www.phearnoc.com" target="_blank">PHEARNOC Digital</a></div>
                     <div className="footerLink numberLink"><a href="tel:021842319">021 842 319</a></div>
                 </div>
+                <div className="footerInfo"><p>Copyright &copy; 2020 House Coffee. All rights reserved.<br />
+                     Website design and build by<a href="https://www.phearnoc.com" target="_blank">&nbsp; PHEARNOC Digital</a></p>
+                     </div>
             </React.Fragment>
 
         )
